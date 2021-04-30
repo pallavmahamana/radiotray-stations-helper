0ea14f1a614a6f6d130dcc67bf3369405c446c7d
Pallav <pallavmahamana@gmail.com>
2021-04-30 16:47:48 +0530

    improved logic, refactor

--- radiotray_stations_helper.py
+++ radiotray_stations_helper.py
@@ -8,22 +8,29 @@ url = "http://www.radiosure.com/rsdbms/stations2.zip"
 response = requests.get(url)
 
 file = zipfile.ZipFile(io.BytesIO(response.content))
-stationsfile = file.read(file.namelist()[0])
+# stationsfile = file.read(file.namelist()[0],'r')
+# stationsfile = file.readlines(file.namelist()[0])
 
-stations  = re.split("(\\t-)*(\\n)",stationsfile)
-stations = [i for i in stations if i is not None]
-stations = [i.split("\t") for i in stations if len(i)>2]
+# stations  = re.split("(\\t-)*(\\n)",stationsfile)
+# stations = [i for i in stations if i is not None]
+# stations = [i.split("\t") for i in stations if len(i)>2]
+# 
 
+file.extract(file.namelist()[0])
+
+with open(file.namelist()[0],'r') as file:
+	stations = file.readlines()
+
+stations = [station.split("\t") for station in stations]
 genres = [i[2] for i in stations]
 genres = set(genres)
-
 genstations = {}
 
 for i in stations:
 	genstations[i[2]] = []
 
 for station in stations:
-	links = [i for i in station if i.find("://")!=-1]
+	links = [r for r in station[6:11] if r.find('http://')>-1]
 	for i,link in enumerate(links):
 		genstations[station[2]].append([station[0]+" ["+str(i)+"]",link])
 
@@ -36,11 +43,11 @@ for genre in genres:
 	genregroup.set("name",genre)
 	for s in genstations[genre]:
 		bookmark = ET.SubElement(genregroup,"bookmark")
-		bookmark.set("name",s[0].decode('UTF-8','ignore').encode('ascii','ignore'))
-		bookmark.set("url",s[1].decode('UTF-8','ignore').encode('ascii','ignore'))
+		bookmark.set("name",s[0].decode('utf-8'))
+		bookmark.set("url",s[1].decode('utf-8'))
 
 tree = ET.ElementTree(bookmarks)
-stationsxml = open("bookmarks.xml","w")
+stationsxml = open("bookmarks.xml","wb")
 tree.write(stationsxml)
 stationsxml.close()
 
